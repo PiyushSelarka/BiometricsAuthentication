b20e9f3a7547831f1f497c8b12b99e0817978ad6
Mind <mind@mac-00019.local>
2018-12-05 15:09:43 +0530

    Initial Commit

---
+++ Biometrics Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Biometrics Authentication
+//
+//  Created by Mac-00017 on 05/12/18.
+//  Copyright © 2018 Mind Inventory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
